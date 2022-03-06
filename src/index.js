ff1d584f9fec992ff563af09477199b599e9ac0b
1234alex1234 <verygoog@yandex.ru>
2022-03-07 01:56:05 +0300

    доделал дз 21

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import regex from "./modules/regex";
 
+regex();
 menu();
 modal();
 timer('15 march 2022');
